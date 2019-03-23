27832a1f0264dd875b29b905c71a8218bd2552c0
marcio.papke <marcio.papke@DESKTOP-6U1BS6P>
2019-03-23 14:11:50 -0300

    criei a classe ClienteContext e o migrations

---
+++ src/TEST_DDD.ApplicationCore/Entity/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TEST_DDD.ApplicationCore.Entity
+{
+   public class Cliente
+    {
+        public Cliente()
+        {
+
+        }
+
+        public int ClienteId { get; set; }
+        public int Nome { get; set; }
+        public int CPF { get; set; }     
+    }
+}
